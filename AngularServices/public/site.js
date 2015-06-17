2b3306b855afca3ce507bf605ea910119e4b1713
Pete Humphrey <wrhumph@gmail.com>
2015-06-16 23:59:59 -0600

    Use ng-include form common menu.

--- AngularServices/public/site.js
+++ AngularServices/public/site.js
@@ -1,13 +1,1 @@
 
-function create_menu(target)
-{
-    var html = "<ul class='list-unstyled'>"
-             + "<li><a href='index.html'>Contents</a></li>"       
-             + "<li><a href='angular1.html'>Basic Module and Controller</a></li>"       
-             + "<li><a href='angular2.html'>Provider Example</a></li>"
-             + "<li><a href='angular3.html'>Provider with Customization</a></li>"
-             + "<li><a href='angular4.html'>Factory Example</a></li>"       
-             + "<li><a href='undefined.html'>Service Example</a></li>"       
-             + "</ul>";
-    $(target).html(html);
-}
